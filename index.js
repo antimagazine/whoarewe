ac5684c6f5c5ad749c2b1bff5c7bd911b61ad8c7
The Ⓐnti-MAGA Zine <253050583+antimagazine@users.noreply.github.com>
2026-01-05 05:27:48 -0800

    Only log on index, add public dir

--- index.js
+++ index.js
@@ -7,8 +7,12 @@ import logger from './logger.js';
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+const publicPath = path.join(process.cwd(), 'public');
+app.use(express.static(publicPath));
+
+const whoamiPath = path.join(process.cwd(), 'whoami.txt');
 app.get('/', logger, (req, res) => {
-  res.download(path.resolve('./whoami.txt'), 'whoami.txt', (err) => {
+  res.download(whoamiPath, 'whoami.txt', (err) => {
     if (err) return res.sendStatus(500);
   });
 });
